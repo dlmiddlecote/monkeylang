a084efbdd94a6f97fa59c12b7ce95deb7ed58a8d
Daniel Middlecote <daniel@ometria.com>
2019-12-14 15:08:40 +0000

    Start with simple AST defns

---
+++ ast/ast.go
@@ -0,0 +1,62 @@
+package ast
+
+import "github.com/dlmiddlecote/monkeylang/token"
+
+// Node defines an interface for all nodes in the AST.
+type Node interface {
+	TokenLiteral() string
+}
+
+// Statement defines the interface for all statement nodes.
+type Statement interface {
+	Node
+	statementNode()
+}
+
+// Expression defines the interface for all expression nodes.
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+// Program is the root node. All programs consist of a slice of Statement(s)
+type Program struct {
+	Statements []Statement
+}
+
+// TokenLiteral implements Node
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	}
+
+	return ""
+}
+
+// LetStatement represents a statement of the form
+// let name = value
+type LetStatement struct {
+	Token token.Token // token.LET
+	Name  *Identifier
+	Value Expression
+}
+
+func (ls *LetStatement) statementNode() {}
+
+// TokenLiteral implements Node
+func (ls *LetStatement) TokenLiteral() string {
+	return ls.Token.Literal
+}
+
+// Identifier represents an identifer and holds the name of the identifier
+type Identifier struct {
+	Token token.Token // token.IDENT
+	Value string
+}
+
+func (i *Identifier) expressionNode() {}
+
+// TokenLiteral implements Node
+func (i *Identifier) TokenLiteral() string {
+	return i.Token.Literal
+}
